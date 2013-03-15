5a1403a0c2e0726f229b76f42a0dcddc0f3a553e
aminovavi <aminovavi@hotmail.com>
2013-03-15 06:50:39 +0200

    Revert "my first commit"
    This reverts commit b4de9fe65dab760f023b85f0d2531ad0d69114dd.

--- hnechasim/function/connect.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","nadlanportal") or die('Unable to establish a DB connection');
-                            //Check connection
-                                mysqli_query("SET NAMES 'utf8'");  
-?>
